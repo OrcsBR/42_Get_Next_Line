fdff07229625d50be1b598758930165412b3fdf9
Paulo Diniz <orcsbr@gmail.com>
2021-09-19 23:16:39 -0300

    first round of testing/debug done and code runs

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: peduardo < peduardo@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 20:55:59 by peduardo          #+#    #+#             */
-/*   Updated: 2021/09/19 03:35:18 by peduardo         ###   ########.fr       */
+/*   Updated: 2021/09/19 23:10:42 by peduardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ static void	partwrite_action(char **result_line, char *curr_read, int i)
 	char	*aux;
 	char	holder;
 
-	if (!result_line)
+	if (!*result_line)
 	{
-		holder = curr_read[i++];
-		curr_read[i] = '\0';
+		holder = curr_read[i + 1];
+		curr_read[i + 1] = '\0';
 		*result_line = ft_strjoin(curr_read, "");
-		curr_read[i] = holder;
+		curr_read[i + 1] = holder;
 	}
 	else
 	{
-		holder = curr_read[i++];
-		curr_read[i] = '\0';
+		holder = curr_read[i + 1];
+		curr_read[i + 1] = '\0';
 		aux = ft_strdup(*result_line);
 		*result_line = ft_strjoin(aux, curr_read);
-		curr_read[i] = holder;
+		curr_read[i + 1] = holder;
 		free(aux);
 	}
 }
@@ -40,17 +40,17 @@ static void	partwrite(char *curr_read, char **result_line, int *bytes_read)
 	int	i;
 
 	i = 0;
-	while(curr_read[i] != '\n' && curr_read[i])
+	while (curr_read[i] != '\n' && curr_read[i])
 		i++;
-	if(!curr_read[i])
-		partwrite_action(&result_line, curr_read, i);
-	else
+	if (!curr_read[i])
+		partwrite_action(result_line, curr_read, i);
+	else if (curr_read[i] == '\n')
 	{
-		partwrite_action(&result_line, curr_read, i);
+		partwrite_action(result_line, curr_read, i);
 		*bytes_read = 0;
 	}
 	i++;
-	curr_read = ft_memmove(curr_read, curr_read[i], BUFFER_SIZE - i); //aqui tinha que ser o endereço de curr_read[i] (&curr_read[i]) - deixa pro debug! E SEM A ATRIBUIÇÃO NO COMEÇO POIS CURR_READ É UMA CONST STRING
+	ft_memmove(curr_read, &curr_read[i], BUFFER_SIZE - i);
 	curr_read[BUFFER_SIZE - i] = '\0';
 }
 
@@ -58,7 +58,7 @@ static void	fullwrite(char *curr_read, char **result_line)
 {
 	char	*aux;
 
-	if (!result_line)
+	if (!*result_line)
 		*result_line = ft_strjoin(curr_read, "");
 	else
 	{
@@ -71,24 +71,23 @@ static void	fullwrite(char *curr_read, char **result_line)
 
 char	*get_next_line(int fd)
 {
-	static char	*curr_read; //diff
+	static char	curr_read[BUFFER_SIZE + 1];
 	char		*result_line;
 	int			bytes_read;
 
 	result_line = NULL;
 	if (*curr_read)
 		partwrite(curr_read, &result_line, &bytes_read);
-	curr_read = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1); // diff
-	if (!curr_read)
-		return (NULL);
 	bytes_read = 1;
-	while(bytes_read)
+	while (bytes_read)
 	{
 		bytes_read = read(fd, curr_read, BUFFER_SIZE);
-		curr_read[BUFFER_SIZE + 1] = '\0'; //aqui eu errei, é [bytes_read], é claro, pois o bytes_read pode ser menor - mas deixa ir pro debug. E não precisa do +1 tmb. Faltou tmb a proteção de retorno de erro (-1) de read, acima. Ver código do JoFelipe.
+		if (bytes_read == -1)
+			return (NULL);
+		curr_read[bytes_read] = '\0';
 		if (bytes_read == BUFFER_SIZE && !ft_strchr(curr_read, '\n'))
 			fullwrite(curr_read, &result_line);
-		else if (bytes_read != BUFFER_SIZE || ft_strchr(curr_read, '\n'))
+		else if (bytes_read)
 			partwrite(curr_read, &result_line, &bytes_read);
 	}
 	return (result_line);
